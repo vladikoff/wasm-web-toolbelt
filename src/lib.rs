f716340a9d1da1a913a5cc42d1925afb54968ad7
vladikoff <vlad.filippov@gmail.com>
2018-12-30 20:00:37 -0500

    Add base64 encode and hex encode/decode

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,8 @@ cfg_if! {
     }
 }
 
+// base64
+
 #[wasm_bindgen]
 pub fn base64decode(encoded: String) -> String {
   // "aGVsbG8gd29ybGQ="
@@ -27,4 +29,30 @@ pub fn base64decode(encoded: String) -> String {
   };
   let bytes = str::from_utf8(&base64val).unwrap();
   bytes.to_owned()
+}
+
+
+#[wasm_bindgen]
+pub fn base64encode(to_encode: String) -> String {
+  let b64 = base64::encode(to_encode.as_bytes());
+  b64.to_string().to_owned()
+}
+
+// hex
+
+#[wasm_bindgen]
+pub fn hexdecode(encoded: String) -> String {
+  let hexval = match hex::decode(&encoded) {
+    Ok(decoded) => decoded,
+    Err(_) => vec![240, 159, 146, 150]
+  };
+  let bytes = str::from_utf8(&hexval).unwrap();
+  bytes.to_owned()
+}
+
+
+#[wasm_bindgen]
+pub fn hexencode(to_encode: String) -> String {
+  let unhexval = hex::encode(to_encode.as_bytes());
+  unhexval.to_string().to_owned()
 }
--- www/index.js
+++ www/index.js
@@ -1,6 +1,6 @@
 import * as wasm from "wasm-web-toolbelt";
 
-//wasm.greet();
+// base64
 
 const base64DecodeTextarea = document.querySelector('#base64decode');
 const base64EncodeTextarea = document.querySelector('#base64encode');
@@ -12,3 +12,34 @@ function base64decode() {
 
 base64DecodeTextarea.addEventListener('paste', base64decode);
 base64DecodeTextarea.addEventListener('change', base64decode);
+
+
+function base64encode() {
+  const result = wasm.base64encode(base64EncodeTextarea.value);
+  base64DecodeTextarea.value = result;
+}
+
+base64EncodeTextarea.addEventListener('paste', base64encode);
+base64EncodeTextarea.addEventListener('change', base64encode);
+
+// hex
+
+const hexDecodeTextarea = document.querySelector('#hexdecode');
+const hexEncodeTextarea = document.querySelector('#hexencode');
+
+function hexdecode() {
+  const result = wasm.hexdecode(hexDecodeTextarea.value);
+  hexEncodeTextarea.value = result;
+}
+
+hexDecodeTextarea.addEventListener('paste', hexdecode);
+hexDecodeTextarea.addEventListener('change', hexdecode);
+
+
+function hexencode() {
+  const result = wasm.hexencode(hexEncodeTextarea.value);
+  hexDecodeTextarea.value = result;
+}
+
+hexEncodeTextarea.addEventListener('paste', hexencode);
+hexEncodeTextarea.addEventListener('change', hexencode);
